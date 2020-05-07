57368265cc4f62626f4841d4cd72c7806c93fada
VolodymyrPst <pasternakv19@gmail.com>
2020-05-07 16:10:09 +0300

    build

---
+++ precache-manifest.7ed12a8d0192d44a090b1823472e3396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179f5a5e30fe9a57729e742f357b4292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1592689f9779592c08a9",
+    "url": "/static/css/main.34954bf3.chunk.css"
+  },
+  {
+    "revision": "dec2a9b9d652248fd7ec",
+    "url": "/static/js/2.8838928e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8838928e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1592689f9779592c08a9",
+    "url": "/static/js/main.e52d4ed7.chunk.js"
+  },
+  {
+    "revision": "9e8bc953ecb4bdbcbe50",
+    "url": "/static/js/runtime-main.e1de3044.js"
+  }
+]);
